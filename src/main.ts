d5a97a14b8cfdc6974f01aa308a89613b5f50fb1
Nazarii Lysak <nazarii.lysak@ua.nestle.com>
2025-12-28 15:45:33 +0200

    Replace tsconfig-paths with module-alias for production runtime

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import 'tsconfig-paths/register';
+import 'module-alias/register';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
